c30b0ffba0078b0666e127820fe804d742ea42e8
Holland, Austin Adams <aaholland@usgs.gov>
2017-10-03 15:21:06 -0600

    Orthoganality tests addded

--- orientation/README.md
+++ orientation/README.md
@@ -7,13 +7,29 @@ The original data files are:
   * _IU.ANMO.10.BH2_
   * _IU.ANMO.10.BHZ_
 
-## Rotation (/rotation)
+## Rotation (_/rotation_)
 
 These examples show the data rotated clockwise by the specified number of degrees.  Each 
 set is in its own directory indicated by the number of degrees that the signal was rotated.
-  * 
+  * _002_
+  *  _005_
+  * _020_
+  * _090_
+  * _110_
+  * _180_
+  * _200_
+  * _270_
+  * _290_
+  * _355_
+  * _358_
   
+## Orthogonality (_/orthogonality_)
+
+These examples use the clockwise angle between the BH1 (held constant) and the BH2.  The
+examples are designed to test both resolution and a wide variety of different cases of non-
+orthogonal components.
+* 060	* 080	* 085	* 087	* 088	* 089	* 090	* 091	* 092	* 093	* 095	* 100	* 120	* 270
   
 ## build_orientation_tests.py
 
-This program created the data examples shown here
+This program created the data examples in these tests.
--- orientation/build_orientation_tests.py
+++ orientation/build_orientation_tests.py
@@ -13,6 +13,7 @@ from obspy.core import *
 import obspy.signal.rotate as rotate
 import numpy as np
 import numpy.testing as npt
+import os
 
 def rms(x):
   """ Calculate the RMS for validating test data sets.  
@@ -23,7 +24,7 @@ def rms(x):
   return np.sqrt(np.mean(np.power(x,2)))
   
 def create_rotation_examples(N,E,angles):
-  for angle in angles
+  for angle in angles:
     r,t=rotate.rotate_ne_rt(N[0].data,E[0].data,angle)
     dir="rotation/%03d" % (angle)
     try:
@@ -32,34 +33,35 @@ def create_rotation_examples(N,E,angles):
       # Directory exists do nothing
       pass
     tr_n=N[0].copy()
-    tr_n.data=r
+    tr_n.data=r.astype('int32')
     tr_e=E[0].copy()
-    tr_e.data=t
-    tr_n.write("rotation/%s/%s" % (dir,tr_n.stats.id),format='MSEED')
-    tr_e.write("rotation/%s/%s" % (dir,tr_e.stats.id),fortmat='MSEED')
+    tr_e.data=t.astype('int32')
+    tr_n.write("%s/%s" % (dir,tr_n.id),format='MSEED')
+    tr_e.write("%s/%s" % (dir,tr_e.id),format='MSEED')
 
 def create_orthoganality_examples(N,E,angles):
   """Create examples where the second horizontal component is rotated out of orthoganality
   by the amount of degrees specified. The angle is the angle difference between the two
   components holding component 1 fixed."""
-  for angle in angles
+  for angle in angles:
     rot_angle=angle-90.
+
     if rot_angle<0.:
-      r,t=rotate.rotate_ne_rt(N[0].data,E[0].data,360.-rot_angle)
+      r,t=rotate.rotate_ne_rt(N[0].data,E[0].data,(360.+rot_angle))
     else:
       r,t=rotate.rotate_ne_rt(N[0].data,E[0].data,rot_angle)
-    dir="orthogalality/%03d" % (angle)
+    dir="orthogonality/%03d" % (angle)
     try:
       os.mkdir(dir)
     except:
       # Directory exists do nothing
       pass
     tr_e=E[0].copy()
-    tr_e.data=t
-    N[0].write("orthogonality/%s/%s" % (dir,N[0].stats.id),format='MSEED')
-    tr_e.write("orthogonality/%s/%s" % (dir,tr_n.stats.id),fortmat='MSEED')    
+    tr_e.data=t.astype('int32')
+    N[0].write("%s/%s" % (dir,N[0].id),format='MSEED')
+    tr_e.write("%s/%s" % (dir,tr_e.id),format='MSEED')    
 
-def check_rotation(N,E,angles):
+def check_rotation_examples(N,E,angles):
   """ Check rotation angles by rotating the partially rotated the rest of the way to 360 
   since obspy doesn't do the negative and compare to original values.  Should be close to 
   zero"""
@@ -77,7 +79,7 @@ def check_rotation(N,E,angles):
       ntests+=1
     except:
       testsfailed+=1
-  print("Check rotation %d tests passed test %d tests failed" % (ntests,testsfailed)
+  print("Check rotation %d tests passed test %d tests failed" % (ntests,testsfailed))
   return True
     
     
@@ -90,7 +92,7 @@ if __name__=='__main__':
   E=read('IU.ANMO.10.BH2')
   simple_rotation_angles=np.array([2.,5.,20.,90.,110.,180.,200.,270.,290.,355.,358.])
   create_rotation_examples(N,E,simple_rotation_angles)
-  check_rotation_examples(N,E,simple_rotation_angles)
+  #check_rotation_examples(N,E,simple_rotation_angles)
   
   internal_angles=([60.,80.,85.,87.,88.,89.,89.5,90.5,91.,92.,93.,95.,100.,120.,270.])
   create_orthoganality_examples(N,E,internal_angles)
